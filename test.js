396fcfa59ef2d70aef6d814002f81f60a710eeaa
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-02-22 21:27:30 -0500

    Publish

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 const lib = require('./');
+process.stdout.write('hi');
+
+lib.write(lib.toColumn(0));
+lib.write(lib.clearLine());
+
+// output should be nothing
+
+lib.setfill('0');
+lib.setwidth(5);
+
+lib.write(Math.floor(Math.random() * 99));
+
+// output should be 000<INT>
